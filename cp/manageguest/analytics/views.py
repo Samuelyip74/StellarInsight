81d76890a9a0bb47d4f5c6a947c9aa6d7cff2493
SamuelYip74 <samuel.yip@live.com>
2019-09-04 14:53:08 +0800

    add

--- cp/manageguest/analytics/views.py
+++ cp/manageguest/analytics/views.py
@@ -1,12 +1,19 @@
 from datetime import datetime, timedelta
 from django.db.models import Count
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 from guest.models import Userbehavior
 
 
 # Create your views here.
 def dashboard(request):
-    one_week_ago = datetime.today() - timedelta(days=3)
+    if 'days' in request.GET:
+        get_days = int(request.GET['days'])
+    else:
+        get_days = 7
+
+    one_week_ago = datetime.today() - timedelta(get_days)
     uni_url = Userbehavior.objects.filter(datetime__gte=one_week_ago).values('url').distinct().count()
     uni_visitors = Userbehavior.objects.filter(datetime__gte=one_week_ago).values('username').distinct().count()
     uni_devices = Userbehavior.objects.filter(datetime__gte=one_week_ago).values('macaddress').distinct().count()
@@ -25,11 +32,27 @@ def dashboard(request):
 
 
 def urldetail(request,username):
-    one_week_ago = datetime.today() - timedelta(days=3)
+    if 'days' in request.GET:
+        get_days = int(request.GET['days'])
+    else:
+        get_days = 7
+
+    one_week_ago = datetime.today() - timedelta(get_days)
     user = username
     urllist = Userbehavior.objects.filter(datetime__gte=one_week_ago,username=user).values('url').distinct()
+
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(urllist,10)
+
+    try:
+        urls = paginator.page(page)
+    except PageNotAnInteger:
+        urls = paginator.page(1)
+    except EmptyPage:
+        urls = paginator.page(paginator.num_pages)
+
     context = {
-        'user' : username,
-        'urllist' : urllist,
+        'urllist' : urls,
     }
     return render(request,"analytics/userdetails.html",context)
